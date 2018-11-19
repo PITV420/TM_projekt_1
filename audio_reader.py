f8aa6a290f36ba6b595b34271e1d806c45e1ef6e
PITV420 <45066955+PITV420@users.noreply.github.com>
2018-11-19 23:59:36 +0100

    Update audio_reader.py

--- audio_reader.py
+++ audio_reader.py
@@ -1,8 +1,6 @@
 import scipy.io.wavfile as wav
-import math
-import numpy as np
 
 def audio_reader(path):
     sampleRate, channel = wav.read(path)
     channel = channel/32768
-    return sampleRate, channel
+    return channel, sampleRate
