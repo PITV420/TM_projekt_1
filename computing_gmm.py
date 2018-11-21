df19a9d9493c571e4022f470576ddda7cfb95ab1
Revzik <b.piekarz1997@gmail.com>
2018-11-21 20:33:27 +0100

    Create GMM models for each parametrized sample
    Created a config file for GMM models.
    Added GMM model creation for each parametrized sound sample according to config.

--- computing_gmm.py
+++ computing_gmm.py
@@ -1,13 +1,54 @@
 from sklearn.mixture import GaussianMixture
 from scipy.stats import norm
 import numpy as np
+import pickle
+import collections
 
 
-def compute_gmm(data_mfcc, feature, components, iterations):
+def loadData(path):
+    file = open(path, 'rb')
+    return pickle.load(file)
 
+
+def loadConfig(path):
+    try:
+        file = open(path, 'r')
+        lines = file.readlines()
+        file.close()
+
+        cfg = {}
+        for line in lines:
+            key, value = line.replace('\n', '').split('=')
+            cfg[key] = value
+
+        cfg['components'] = int(cfg['components'])
+        cfg['max_iterations'] = int(cfg['max_iterations'])
+        cfg['toleration'] = float(cfg['toleration'])
+        if not cfg['covariance_type'] == 'diag' or\
+           not cfg['covariance_type'] == 'full' or\
+           not cfg['covariance_type'] == 'tied' or\
+           not cfg['covariance_type'] == 'spherical':
+            cfg['covariance_type'] = 'diag'
+
+    except Exception as e:
+        print('Error:', e, '// using default config')
+        cfg = {
+            'components': 8,
+            'max_iterations': 30,
+            'toleration': 0.001,
+            'covariance_type': 'diag',
+        }
+
+    return cfg
+
+
+def compute_gmm(data_mfcc, cfg):
+    return GaussianMixture(n_components=cfg['components'], covariance_type=cfg['covariance_type'],
+                               max_iter=cfg['max_iterations'], tol=cfg['toleration']).fit(data_mfcc)
+
+"""
     gauss_data = 0
-    gmm_data = GaussianMixture(n_components=components, covariance_type='diag', max_iter=iterations).fit(data_mfcc)
-
+    
     means_data = gmm_data.means_[:, feature]
     weights_data = gmm_data.weights_
     covs_data = gmm_data.covariances[:, feature]
@@ -20,3 +61,13 @@ def compute_gmm(data_mfcc, feature, components, iterations):
         gauss_data = gauss_data + norm.pdf(x, means_data[i], covs_data[i]) * weights_data[i]
 
     return gauss_data
+"""
+
+
+data = loadData('files/parametrized.p')
+config = loadConfig('config/gmm.cfg')
+
+gmm_models = {}
+for key in data:
+    gmm_models[key] = compute_gmm(data[key][0], config)
+gmm_models = collections.OrderedDict(sorted(gmm_models.items()))
