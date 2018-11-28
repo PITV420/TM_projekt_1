9e05bd961a93379868a8b89bef8d57525f859395
Revzik <b.piekarz1997@gmail.com>
2018-11-29 00:20:44 +0100

    Update cross_valid.py

--- cross_valid.py
+++ cross_valid.py
@@ -6,7 +6,8 @@ from sklearn.model_selection import cross_val_score
 import numpy as np
 import math
 import pickle
-
+from sklearn.mixture import GaussianMixture
+import computing_gmm
 
 def loadData(pathMFCC, pathGMM):
     fileMFCC = open(pathMFCC, 'rb')
@@ -14,32 +15,43 @@ def loadData(pathMFCC, pathGMM):
     return pickle.load(fileMFCC), pickle.load(fileGMM)
 
 
-def eachDigitTest(data, GMM):
+def eachDigitTest(data, config):
     aux = {}
     rr = {}
+    testVal = {}
+    trainVal = {}
     for key1 in data:
         for key2 in data[key1]:
             if key2 == list(data[key1].keys())[0]:
                 aux = data[key1][key2]
             aux = np.concatenate((aux, data[key1][key2]), axis=0)
-        rr[key1] = calcRR(GMM[key1].score_samples(aux), aux)
-    return rr
-
-
-
-
-def calcRR(gmm_samples, mfcc_matrix):
-
-    kf = KFold(n_splits=22)
-    rr={}
-
-    lasso_cv = LassoCV(alphas=mfcc_matrix, cv=kf.get_n_splits(mfcc_matrix))
-    for k, (train, test) in enumerate(kf.split(mfcc_matrix, gmm_samples)):
-        rr = np.concatenate(rr, lasso_cv.fit(mfcc_matrix[train], gmm_samples[train]).score(mfcc_matrix[test], gmm_samples[test]))
+        rr1, train1, test1 = calcRR(aux, config)
+        #tutaj trzeba zapis zrobić
 
     return rr
 
 
+
+
+def calcRR(mfcc_matrix, cfg):
+
+    est = GaussianMixture(n_components=cfg['components'], covariance_type=cfg['covariance_type'], max_iter=cfg['max_iterations'], tol=cfg['toleration'])
+    kf = KFold(n_splits=11, random_state=None)
+
+    rr = {}
+    train = {}
+    test = {}
+
+    for k, (train, test) in enumerate(kf.split(X=mfcc_matrix)):
+
+        rr = np.round(np.exp(est.fit(mfcc_matrix[train]).score_samples(mfcc_matrix[test])), 4)
+        rr = np.mean(rr)
+
+        print(rr)
+
+    return rr, train, test
+
+config = computing_gmm.loadConfig('config/gmm.cfg')
 MFCC, GMM = loadData('files/parametrized.p', 'files/digits_gmm.p')
 
-print(eachDigitTest(MFCC, GMM))
+data = eachDigitTest(MFCC, config)
