6a99a0f82f817770addab874db8dc8b49917f563
Revzik <b.piekarz1997@gmail.com>
2018-11-29 02:23:49 +0100

    Chujowe to

--- cross_valid.py
+++ cross_valid.py
@@ -3,6 +3,9 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import pickle
 
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
 
 def loadConfig(path):
     try:
@@ -18,9 +21,9 @@ def loadConfig(path):
         cfg['components'] = int(cfg['components'])
         cfg['max_iterations'] = int(cfg['max_iterations'])
         cfg['toleration'] = float(cfg['toleration'])
-        if not cfg['covariance_type'] == 'diag' or\
-           not cfg['covariance_type'] == 'full' or\
-           not cfg['covariance_type'] == 'tied' or\
+        if not cfg['covariance_type'] == 'diag' and\
+           not cfg['covariance_type'] == 'full' and\
+           not cfg['covariance_type'] == 'tied' and\
            not cfg['covariance_type'] == 'spherical':
             cfg['covariance_type'] = 'diag'
 
@@ -36,35 +39,98 @@ def loadConfig(path):
     return cfg
 
 
-def loadData(pathMFCC, pathGMM):
+def loadData(pathMFCC, pathMFCC_labels):
     fileMFCC = open(pathMFCC, 'rb')
-    fileGMM = open(pathGMM, 'rb')
-    return pickle.load(fileMFCC), pickle.load(fileGMM)
+    fileMFCC_labels = open(pathMFCC_labels, 'rb')
+    return pickle.load(fileMFCC), pickle.load(fileMFCC_labels)
 
 
-def validate(data, cfg):
+def getSets(data):
+    train_idx, test_idx = train_test_split(range(len(data[0])), test_size=0.2, random_state=None)
+    train_set = []
+    test_set = []
+    for i in range(10):
+        train = []
+        test = []
+        for j in train_idx:
+            train.append(data[i][j])
+        for j in test_idx:
+            test.append(data[i][j])
+        train_set.append(train)
+        test_set.append(test)
+
+    return train_set, test_set
+
+
+def analyze(mfcc, gmm, digit):
+    """HELPER FUNCTION"""
+    m = 1
+    plt.hist(mfcc[:, m], bins=70, density=True, rwidth=0.90)
+
+    left, right = plt.xlim()
+    x = np.linspace(left, right, 100)
+    gauss = 0
+    for i in range(len(gmm.weights_)):
+        y = norm.pdf(x, gmm.means_[i, m], gmm.covariances_[i, m])*gmm.weights_[i]
+        plt.plot(x, y, '#EE8000')
+        gauss += y
+    plt.plot(x, gauss, '#DD0000')
+    plt.xlim((left, right))
+    plt.title('"' + str(digit) + '" - parametr c' + str(m))
+    plt.show()
+    print("Aic:", gmm.aic(mfcc))
+
+
+def trainModels(train, cfg):
     estimator = GaussianMixture(n_components=cfg['components'], covariance_type=cfg['covariance_type'],
                                max_iter=cfg['max_iterations'], tol=cfg['toleration'])
+    models = []
+    for i in range(len(train)):
+        data = train[i][0]
+        for j in range(1, len(train[i])):
+            data = np.concatenate((data, train[i][j]), axis=0)
+        models.append(estimator.fit(data))
+        #analyze(data, models[i], i)
 
-    models = {}
-    test_set = {}
+    return models
+
+
+def validateModels(models, test):
+    matrix = np.empty((10, 10), dtype=float)
     for i in range(10):
-        train, test = train_test_split(data[i], test_size=0.2, random_state=None)
-        test_set[i] = test
-        train_set = train[0]
-        for j in range(1, len(train)):
-            train_set = np.concatenate((train_set, train[i]), axis=0)
-        models[i] = estimator.fit(train_set)
-
-    for i in test_set:
-        print(models[2].score(test_set[i][0]))
-
-    return models, test_set
+        for j in range(10):
+            matrix[i][j] = models[i].score(test[j][1])
+    return matrix
 
 
 config = loadConfig('config/gmm.cfg')
 MFCC, MFCC_labels = loadData('files/parametrized.p', 'files/mfcc_matrix_scheme.p')
-#models, test_set = validate(MFCC, config)
 
+train_, test_ = getSets(MFCC)
+models_ = trainModels(train_, config)
+matrix_ = validateModels(models_, test_)
+
+"""
 for i in MFCC_labels:
     print(i)
+
+train_idx, test_idx = train_test_split(range(22), test_size=0.2, random_state=None)
+print(train_idx)
+print(test_idx)
+train_set = []
+test_set = []
+for i in range(10):
+    train = []
+    test = []
+    for j in train_idx:
+        train.append(MFCC_labels[i][j])
+    for j in test_idx:
+        test.append(MFCC_labels[i][j])
+    train_set.append(train)
+    test_set.append(test)
+
+for i in train_set:
+    print(i)
+for i in test_set:
+    print(i)
+"""
